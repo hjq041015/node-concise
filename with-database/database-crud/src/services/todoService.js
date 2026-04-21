065b68891d6f8a66ecbf8cb6036b2d306f8ef123
zhiyi <huangjiaqi041015@gmail.com>
2026-04-21 23:09:58 +0800

    complete database-crud

---
+++ with-database/database-crud/src/services/todoService.js
@@ -0,0 +1,36 @@
+import Todo from "../models/todoModel.js";
+
+export async function getAllTodos() {
+  const todos = await Todo.findAll();
+  return todos;
+}
+
+export async function getTodoById(todoId) {
+  const todos = await Todo.findOne({
+    where: {
+      id: todoId,
+    },
+  });
+  return todos;
+}
+
+export async function deleteTodoById(todoId) {
+  await Todo.destroy({
+    where: {
+      id: todoId,
+    },
+  });
+}
+
+export async function createTodo(addTodo) {
+  const addedTodo = await Todo.create(addTodo);
+  return addedTodo;
+}
+
+export async function updateTodo(updateTodo) {
+  await Todo.update(updateTodo, {
+    where: {
+      id: updateTodo.id,
+    },
+  });
+}
