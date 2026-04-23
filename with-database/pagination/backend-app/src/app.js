0a4dbd319afdd8c891424feab2908c54d31039ea
zhiyi <huangjiaqi041015@gmail.com>
2026-04-23 20:08:24 +0800

    add rate-limit

--- with-database/pagination/backend-app/src/app.js
+++ with-database/pagination/backend-app/src/app.js
@@ -1,12 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import todoRouter from './routes/todoRoute.js';
+import express from "express";
+import cors from "cors";
+import todoRouter from "./routes/todoRoute.js";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
+const limiter = rateLimit({
+  windowMs: 1000, // 1 second
+  limit: 10, // 请求的次数
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
 
 app.use(cors());
 app.use(express.json());
+app.use(limiter);
 
-app.use('/v1', todoRouter);
+app.use("/v1", todoRouter);
 
 export default app;
